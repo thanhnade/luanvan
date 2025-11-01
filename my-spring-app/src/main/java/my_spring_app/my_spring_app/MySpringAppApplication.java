f31861e1db1b86911b82e16161ced60c0f230c83
nguyenbao <bao2104797@gmail.com>
2025-11-01 13:13:18 +0700

    create my-react-app and my-spring-app

---
+++ my-spring-app/src/main/java/my_spring_app/my_spring_app/MySpringAppApplication.java
@@ -0,0 +1,13 @@
+package my_spring_app.my_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringAppApplication.class, args);
+	}
+
+}
---
+++ my-spring-app/src/test/java/my_spring_app/my_spring_app/MySpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package my_spring_app.my_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
