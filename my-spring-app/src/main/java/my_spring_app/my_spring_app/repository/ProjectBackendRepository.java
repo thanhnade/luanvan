50032a9a0400a56ed2b2a62274f2bed392234650
nguyenbao <bao2104797@gmail.com>
2025-11-14 16:10:26 +0700

    backend: api deploy backend

--- my-spring-app/src/main/java/my_spring_app/my_spring_app/repository/ProjectBackendRepository.java
+++ my-spring-app/src/main/java/my_spring_app/my_spring_app/repository/ProjectBackendRepository.java
@@ -1,17 +1,17 @@
-//package my_spring_app.my_spring_app.repository;
-//
-//import my_spring_app.my_spring_app.entity.ProjectBackendEntity;
-//import my_spring_app.my_spring_app.entity.UserEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface ProjectBackendRepository extends JpaRepository<ProjectBackendEntity, Long> {
-//    boolean existsByDomainNameSystem(String domainNameSystem);
-//
-//    // Tìm tất cả backend projects của một user
-//    List<ProjectBackendEntity> findByUser(UserEntity user);
-//}
-//
+package my_spring_app.my_spring_app.repository;
+
+import my_spring_app.my_spring_app.entity.ProjectBackendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectBackendRepository extends JpaRepository<ProjectBackendEntity, Long> {
+
+    boolean existsByDomainNameSystem(String domainNameSystem);
+
+    @Query("SELECT COUNT(e) > 0 FROM ProjectBackendEntity e WHERE e.uuid_k8s = :uuid_k8s")
+    boolean existsByUuid_k8s(@Param("uuid_k8s") String uuid_k8s);
+}
+
--- my-spring-app/src/main/java/my_spring_app/my_spring_app/service/ProjectBackendService.java
+++ my-spring-app/src/main/java/my_spring_app/my_spring_app/service/ProjectBackendService.java
@@ -1,13 +1,10 @@
-//package my_spring_app.my_spring_app.service;
-//
-//import my_spring_app.my_spring_app.dto.reponse.DeployBackendResponse;
-//import my_spring_app.my_spring_app.dto.reponse.ListProjectBackendResponse;
-//import my_spring_app.my_spring_app.dto.request.DeployBackendRequest;
-//
-//public interface ProjectBackendService {
-//
-//    ListProjectBackendResponse getAllProjectBackends();
-//
-//    DeployBackendResponse deploy(DeployBackendRequest request);
-//}
-//
+package my_spring_app.my_spring_app.service;
+
+import my_spring_app.my_spring_app.dto.reponse.DeployBackendResponse;
+import my_spring_app.my_spring_app.dto.request.DeployBackendRequest;
+
+public interface ProjectBackendService {
+
+    DeployBackendResponse deploy(DeployBackendRequest request);
+}
+
